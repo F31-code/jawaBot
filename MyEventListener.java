67b8eb22375196180c3b4a68bf55e53b1653943d
CKStudios2018 <75259904+CKStudios2018@users.noreply.github.com>
2021-03-17 09:18:10 +0100

    Create MyEventListener.java
    Listener for the commands?

---
+++ MyEventListener.java
@@ -0,0 +1,11 @@
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class onMessageReceived(MessageReceivedEvent) {
+    
+}
