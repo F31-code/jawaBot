683760a215dad73f1e7a864a92c1cdf8e1b345c3
F31-code <81227546+F31-code@users.noreply.github.com>
2021-03-24 13:55:26 +0100

    Update Main.java
    Cleaned some stuff

--- Main.java
+++ Main.java
@@ -2,16 +2,21 @@ import net.dv8tion.jda.core.*;
 
 public class Main {
 
-    private static String token = "Bot Token Here";
-    public static void main(String[] args) throws Exception{
-
-        try {
-            JDA api = new JDABuilder(AccountType.BOT).setToken(token).build();
-            api.addEventListener(new MyEventListner() );
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static JDA jda;
+    
+    public static void main(String[] args) throws LoginException {
+        jda = new JDABuilder()
+            .setToken()
+            .setActivity(Activity.playing("jawaBot"))
+            .addEventListeners(new EventListener())
+            .build();
+    }
+    
+    pubic class EventListener extends ListenerAdapter {
+        if (event.getAuthor().isBot()){
+            return;
         }
-
+        System.out.printIn("Some nerd said " + event.getMessage().getContentRaw());
     }
 
 }
